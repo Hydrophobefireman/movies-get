9e950574c5196cc7bebcd2fef38eb966a4573db6
me <bhavesh.pareek36@gmail.com>
2018-08-18 02:20:29 +0530

    update

--- flask_tools/__init__.py
+++ flask_tools/__init__.py
@@ -51,35 +51,8 @@ class flaskUtils(object):
         @app.after_request
         def after_req_headers(res):
             res.headers["X-Process-Time"] = time.time() - request.process_time
-            accept_encoding = request.headers.get("Accept-Encoding", "")
             res.headers["X-UID"] = str(uuid.uuid4())
-            if (
-                res.mimetype not in config["COMPRESS_MIMETYPES"]
-                or "br" not in accept_encoding.lower()
-                or not 200 <= res.status_code < 300
-                or (
-                    res.content_length is not None
-                    and res.content_length < config["COMPRESS_MIN_SIZE"]
-                )
-                or "Content-Encoding" in res.headers
-            ):
-                return res
-            res.direct_passthrough = False
-            uncompressed_length = res.content_length
-            res.set_data(brotli_content(res))
-            res.headers["Content-Encoding"] = "br"
-            res.headers["Content-Length"] = res.content_length
-            res.headers["X-Compression-Percentage"] = int(
-                (res.content_length / uncompressed_length) * 100
-            )
-            vary = res.headers.get("Vary")
-            if vary:
-                if "accept-encoding" not in vary.lower():
-                    res.headers["Vary"] = "%s, Accept-Encoding" % (vary)
-            else:
-                res.headers["Vary"] = "Accept-Encoding"
-
-            return res
+            return res 
 
         @app.route("/favicon.ico")
         def send_fav():
@@ -88,9 +61,3 @@ class flaskUtils(object):
                 "favicon.ico",
                 mimetype="image/x-icon",
             )
-
-
-def brotli_content(response):
-    data = response.get_data()
-    deflated = brotli.compress(data)
-    return deflated
